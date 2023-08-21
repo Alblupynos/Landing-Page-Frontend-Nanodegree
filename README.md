32289f1dd83a854587694011fa27a0748539fb8b
Albina <alblupynos@gmail.com>
2023-08-21 18:10:27 +0300

    Add JS to manipulating the DOM.

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # Landing Page Project
 
-## Instructions
+## Project description
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+This project aims to give you real-world scenarios of manipulating the DOM. The functionality you will be using serves
+two purposes: to prepare you for appending dynamically added data to the DOM, and to show you how javascript can improve
+the usability of an otherwise static site. This project barely touches the surface of what is possible, but it does use
+some incredibly common events, methods, and logic.
 
-To get started, open `js/app.js` and start building out the app's functionality
+For this project, refactor and test as much as possible while you are building. You should figure for every piece of
+functionality you add, you will likely spend just as much time testing and refactoring your code. If it takes you 3
+hours to figure out the logic, it should likely take you another 3 hours determining that you wrote the best code
+possible. As your skills improve, this process will feel more natural. Make sure to remove any debugging code from your
+final submission.
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+## Usage
+
+For educational purposes.
+
+## Tools
+
+**Javascript, HTML, CSS.**
--- js/app.js
+++ js/app.js
@@ -1,62 +1,81 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
-
+ *
+ */
+const navList = document.getElementById('navbar__list');
+const sections = document.querySelectorAll('section');
 
 /**
  * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
-
+function createNavListItems() {
+    for (const section of sections) {
+        const navListItem = document.createElement("li");
+        navListItem.innerHTML = `<h3 data-nav="${section.id}" id="${section.dataset.nav}" class="navbar__item">${section.dataset.nav}</h3>`;
+        navList.appendChild(navListItem);
+    }
+}
 
 // Add class 'active' to section when near top of viewport
 
-
-// Scroll to anchor ID using scrollTO event
-
+function makeActive() {
+    for (const section of sections) {
+        const menuElement = document.getElementById(`${section.dataset.nav}`);
+        const box = section.getBoundingClientRect();
+        section.classList.remove("your-active-class");
+        menuElement.classList.remove("active-class");
+        if (box.top >= -300 && box.top <= 200) {
+            section.classList.add("your-active-class");
+            menuElement.classList.add("active-class");
+        }
+    }
+}
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
+document.addEventListener('DOMContentLoaded', createNavListItems);
 
 // Scroll to section on link click
+navList.addEventListener('click', event => {
+    event.preventDefault();
+    document.getElementById(`${event.target.dataset.nav}`).scrollIntoView({behavior: "smooth"});
+});
+
 
 // Set sections as active
-
-
+window.onscroll = function () {
+    makeActive()
+};
+/**
+ * End Events
+ *
+ */
